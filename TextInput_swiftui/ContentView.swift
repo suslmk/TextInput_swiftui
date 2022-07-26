57922b98fc387a50f29cc31381cd85069f1a263a
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-27 06:45:26 +0900

    텍스트 인풋 로그인 창

--- TextInput_swiftui/ContentView.swift
+++ TextInput_swiftui/ContentView.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 10){
+            
+            HStack{
+                TextField("사용자 이름", text: $username)
+                    .autocapitalization(.none) // 소문자로
+                    .disableAutocorrection(true)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button(action: {
+                    self.username = ""
+                }){
+                    if(self.username.count > 0 ){
+                        Image(systemName: "multiply.circle.fill")
+                            .font(.system(size: 25))
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            
+            HStack{
+                SecureField("비밀번호", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    self.password = ""
+                }){
+                    if(self.password.count > 0) {
+                        Image(systemName: "multiply.circle.fill")
+                            .font(.system(size: 25))
+                            .foregroundColor(.secondary)
+                    }
+                    
+                }
+            }
+            
+            Text("입력한 비번 : \(password)")
+            
+            
+        }.padding(.horizontal, 50)
+        
     }
 }
 
