a8525b4d5f46aca9654937ffa5da22d48de0fe07
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-27 06:03:24 +0900

    Initial Commit

---
+++ TextInput_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextInput_swiftui
+//
+//  Created by minkyuLee on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextInput_swiftui/TextInput_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInput_swiftuiApp.swift
+//  TextInput_swiftui
+//
+//  Created by minkyuLee on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct TextInput_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
